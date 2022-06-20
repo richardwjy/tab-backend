b77cb1f2fd02f95f24680ed7417fb21f13e75378
Richard <richard_huang0309@yahoo.com>
2022-06-20 21:35:26 +0700

    Adding pg tutorial

--- routes/Tutorial/index.js
+++ routes/Tutorial/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { axiosGet, axiosPost } = require("../../services/axios");
+const { Client } = require('pg');
 
 router.get('/how-to-get', async (req, res) => {
     // console.log({ token: OAuth({ script: 323, deploy: 1, httpmethod: "GET" }) });
@@ -23,6 +24,27 @@ router.post('/how-to-post', async (req, res) => {
         console.log(err.message);
         console.log(err.response);
         return res.send("Error!");
+    }
+})
+
+router.get('/how-to-connect-psql', async (req, res) => {
+    // Notes Richard (20-June-2022)
+    // Silahkan di enchance jika mau, contoh enhancement: membuat services yang hanya return data/error instead of code panjang begini.
+    // Jika memang pengen dipindah jadi service, pikirin semua scenario termasuk kluarin ID ketika insert data dll
+    try {
+        const client = new Client();
+        client.connect();
+        client.query(`SELECT * FROM MS_USERS ORDER BY id ASC`, (error, results) => {
+            if (error) {
+                console.error(error);
+                return res.status(200).json({ status: false, message: error })
+            }
+            client.end();
+            return res.status(200).json(results.rows);
+        })
+    } catch (err) {
+        console.error(err);
+        return res.status(200).json({ status: false, message: err })
     }
 })
 
