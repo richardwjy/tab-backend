c702d8df4395ef35d9d7e49bc0b4955f28deacc5
Richard <richard_huang0309@yahoo.com>
2022-06-20 22:01:26 +0700

    Updating documentation on tutorial route

--- routes/Tutorial/index.js
+++ routes/Tutorial/index.js
@@ -5,6 +5,7 @@ const { Client } = require('pg');
 
 router.get('/generate-oauth', async (req, res) => {
     // Silahkan diisi query parameters
+    // http://localhost:5000/v1/api/tutorial/generate-oauth?script=323&deploy=1
     const { query } = req;
     const stringified = JSON.stringify({ token: OAuth({ ...query, httpmethod: req.method }) });
     console.log(JSON.parse(stringified))
@@ -12,8 +13,7 @@ router.get('/generate-oauth', async (req, res) => {
 })
 
 router.get('/how-to-get', async (req, res) => {
-    // console.log({ token: OAuth({ script: 323, deploy: 1, httpmethod: "GET" }) });
-    // return res.json({ token: OAuth({ script: 323, deploy: 1, httpmethod: "GET" }) });
+    // http://localhost:5000/v1/api/tutorial/how-to-get
     try {
         const { data } = await axiosGet({ script: 323, deploy: 1 })
         return res.json(data);
@@ -25,6 +25,7 @@ router.get('/how-to-get', async (req, res) => {
 })
 
 router.post('/how-to-post', async (req, res) => {
+    // http://localhost:5000/v1/api/tutorial/how-to-post
     try {
         const { data } = await axiosPost({ params: { script: 323, deploy: 1 }, body: { someJsonString: "Hati hati di jalan!", someJsonId: 22, someJsonBoolean: true } })
         return res.json(data);
@@ -37,6 +38,8 @@ router.post('/how-to-post', async (req, res) => {
 })
 
 router.get('/how-to-connect-psql', async (req, res) => {
+    // http://localhost:5000/v1/api/tutorial/how-to-connect-psql
+
     // Notes Richard (20-June-2022)
     // Silahkan di enchance jika mau, contoh enhancement: membuat services yang hanya return data/error instead of code panjang begini.
     // Jika memang pengen dipindah jadi service, pikirin semua scenario termasuk kluarin ID ketika insert data dll
