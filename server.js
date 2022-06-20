c234a257399344042bae0860e264c538168c554d
Richard <richard_huang0309@yahoo.com>
2022-06-20 19:17:08 +0700

    Adding oauth services to generate OAuth Token

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const dotnev = require('dotenv')
 dotnev.config()
+
+const OAuth = require('./services/oauth');
 const app = express()
-const port = 3000
+const PORT = 5000
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    console.log({ token: OAuth({ script: 323, deploy: 1, httpmethod: "GET" }) });
+    return res.json({ token: OAuth({ script: 323, deploy: 1, httpmethod: "GET" }) });
 })
 
-app.listen(port, () => {
-    console.log(`Anteraja app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Anteraja app listening on port ${PORT}`)
 })
